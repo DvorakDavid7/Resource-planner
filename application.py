b736212772ff70966e8e8c2cb75d0864e430a4a9
Dvořák David <ddvorak@trask.cz>
2019-12-19 14:33:20 +0100

    napojení na ostrou dtb 2

--- application.py
+++ application.py
@@ -2,4 +2,4 @@ from planner import app
 
 
 if __name__ == "__main__":
-    app.run(debug = True, port = 80)  # parametr debud = True okamžité provedení změny
+    app.run(debug = False, port = 80)  # parametr debud = True okamžité provedení změny
